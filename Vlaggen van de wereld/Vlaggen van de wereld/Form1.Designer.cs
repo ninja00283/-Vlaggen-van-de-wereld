b8ff54243783ca1ce8da0d4d9442eefa94859ad3
jan bos <max.bos217@gmail.com>
2018-09-05 14:11:49 +0200

    added flags and buttons

--- Vlaggen van de wereld/Vlaggen van de wereld/Form1.Designer.cs
+++ Vlaggen van de wereld/Vlaggen van de wereld/Form1.Designer.cs
@@ -28,13 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.FlagBox = new System.Windows.Forms.PictureBox();
+            this.AwnserBox = new System.Windows.Forms.GroupBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            ((System.ComponentModel.ISupportInitialize)(this.FlagBox)).BeginInit();
+            this.AwnserBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // FlagBox
+            // 
+            this.FlagBox.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.FlagBox.Location = new System.Drawing.Point(12, 12);
+            this.FlagBox.Name = "FlagBox";
+            this.FlagBox.Size = new System.Drawing.Size(458, 426);
+            this.FlagBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.FlagBox.TabIndex = 0;
+            this.FlagBox.TabStop = false;
+            this.FlagBox.Click += new System.EventHandler(this.PictureClick);
+            // 
+            // AwnserBox
+            // 
+            this.AwnserBox.Controls.Add(this.radioButton6);
+            this.AwnserBox.Controls.Add(this.radioButton7);
+            this.AwnserBox.Controls.Add(this.radioButton8);
+            this.AwnserBox.Controls.Add(this.radioButton9);
+            this.AwnserBox.Controls.Add(this.radioButton10);
+            this.AwnserBox.Controls.Add(this.radioButton5);
+            this.AwnserBox.Controls.Add(this.radioButton4);
+            this.AwnserBox.Controls.Add(this.radioButton3);
+            this.AwnserBox.Controls.Add(this.radioButton2);
+            this.AwnserBox.Controls.Add(this.radioButton1);
+            this.AwnserBox.Location = new System.Drawing.Point(476, 12);
+            this.AwnserBox.Name = "AwnserBox";
+            this.AwnserBox.Size = new System.Drawing.Size(312, 426);
+            this.AwnserBox.TabIndex = 1;
+            this.AwnserBox.TabStop = false;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton1.Location = new System.Drawing.Point(7, 22);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(17, 16);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton2.Location = new System.Drawing.Point(7, 145);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(17, 16);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton3.Location = new System.Drawing.Point(7, 104);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(17, 16);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton4.Location = new System.Drawing.Point(7, 63);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(17, 16);
+            this.radioButton4.TabIndex = 3;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton5.Location = new System.Drawing.Point(7, 186);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(17, 16);
+            this.radioButton5.TabIndex = 4;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton6.Location = new System.Drawing.Point(7, 391);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(17, 16);
+            this.radioButton6.TabIndex = 9;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton7.Location = new System.Drawing.Point(7, 268);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(17, 16);
+            this.radioButton7.TabIndex = 8;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton8.Location = new System.Drawing.Point(7, 309);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(17, 16);
+            this.radioButton8.TabIndex = 7;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton9.Location = new System.Drawing.Point(7, 350);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(17, 16);
+            this.radioButton9.TabIndex = 6;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F);
+            this.radioButton10.Location = new System.Drawing.Point(7, 227);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(17, 16);
+            this.radioButton10.TabIndex = 5;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.AwnserBox);
+            this.Controls.Add(this.FlagBox);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.FlagBox)).EndInit();
+            this.AwnserBox.ResumeLayout(false);
+            this.AwnserBox.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox FlagBox;
+        private System.Windows.Forms.GroupBox AwnserBox;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton5;
     }
 }
 
--- Vlaggen van de wereld/Vlaggen van de wereld/Form1.cs
+++ Vlaggen van de wereld/Vlaggen van de wereld/Form1.cs
@@ -2,19 +2,81 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
 namespace Vlaggen_van_de_wereld
 {
     public partial class Form1 : Form
     {
+        
+
+        /// <summary>
+        /// 
+        /// Get the images from the map.
+        /// 
+        /// </summary>
         public Form1()
         {
             InitializeComponent();
+
+            initializeImages();
+        }
+
+        private string[] files;
+
+        /// <summary>
+        /// Grab directories in your images directory.
+        /// </summary>
+        private void initializeImages()
+        {
+            string appRoot = System.IO.Path.GetDirectoryName(Application.ExecutablePath);
+            files = System.IO.Directory.GetFiles(appRoot + @"\Flags");
+        }
+
+        /// <summary>
+        /// Randomize the image array and set the image to the first image in the array.
+        /// </summary>
+        private void setImage()
+        {
+            Random rnd = new Random();
+            files = files.OrderBy(x => rnd.Next()).ToArray();
+            FlagBox.ImageLocation = files[0];
+            Debug.WriteLine(files[0]);
+        }
+
+
+        /// <summary>
+        /// Get the name of the image in the specfied directory.
+        /// </summary>
+        /// <param name="Directory"></param>
+        /// <returns></returns>
+        private string getFileName(string Directory) {
+            string Name = "";
+
+            Name = Directory.Substring(Directory.LastIndexOf("\\"),Directory.Length);
+
+            return Name;
+        }
+
+        private void createAwnsers(int Awnsers)
+        {
+            for (int i = 0; i < Awnsers; i++)
+            {
+
+            }
+
+        }
+
+        private void PictureClick(object sender, EventArgs e)
+        {
+            setImage();
         }
     }
 }
